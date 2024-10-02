d11ba239ab7eb97f4b36cb6baf99e006c098b6dd
Prakhar Kumar Srivastava <prakhar25789@gmail.com>
2024-10-03 03:39:12 +0530

    Url Fixes

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path={'/'} element={<CreateRoom />}  />
-      <Route path={'/lobby/:type'} element={<Lobby />} />
+      <Route path={'/lobby/:type/*'} element={<Lobby />} />
       <Route path={'/join/:id'} element={<Room />} />
     </Routes>
   )
